2d9f0c043edba50d0bc53a53c44ddf7e820f7b6f
Reham Muhammad <reham.muhammad.helmy@gmail.com>
2021-08-02 05:26:55 +0200

    Error handling for Express

--- server/app.js
+++ server/app.js
@@ -10,22 +10,18 @@ app.use('/graphql',graphqlHTTP({
 
 }));
 
+
 app.listen(7000, () => {
 console.log('iam now listening for requests on port 7000');
 });
 
-//Handing the error while getting the main page.
 
-// app.get('/', function (req, res) {
-//     throw new Error('BROKEN') // Express catches this err by itself.
-// })
 
-// app.get('/', function (req, res, next) {
-//     fs.readFile('/file-does-not-exist', function (err, data) {
-//     if (err) {
-//     next(err) // Passing errors to Express.
-//     } else {
-//         res.send(data)
-//     }
-//     })
-// })
+
+
+//Handing the error while getting the main page (/) instead of (Cannot GET /) message
+
+app.get('/', function (req, res) {
+    throw new Error("The page has been BROKEN we're working on that now ") // Express catches this err by itself.
+})
+
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,3 +1,5 @@
+// I build a very basic schema to avoid graphQl error (Middlware options must contain a schema)
+
 const graphql = require('graphql');
 
 const {GraphQLObjectType} = graphql;
